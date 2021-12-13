c74be8bcd1e6d8e22b47561bb17adf66e57c3da0
WindzoR <windzr67@gmail.com>
2021-12-13 21:38:39 +0300

    Get JSON response AllEmployees

---
+++ src/main/java/com/petproject/spring/rest/dao/EmployeeDAO.java
@@ -0,0 +1,18 @@
+package com.petproject.spring.rest.dao;
+
+
+
+import com.petproject.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
---
+++ src/main/java/com/petproject/spring/rest/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.petproject.spring.rest.service;
+
+
+
+import com.petproject.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
