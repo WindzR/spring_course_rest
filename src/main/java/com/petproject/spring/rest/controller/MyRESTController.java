2b36b0c9f8c294676e22e903fc497de5e38cca42
WindzoR <windzr67@gmail.com>
2021-12-14 16:10:08 +0300

    Develop global Exception Handler 'EmployeeGlobalExceptionHandler' - transfer Exceptions

--- src/main/java/com/petproject/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/petproject/spring/rest/controller/MyRESTController.java
@@ -1,12 +1,9 @@
 package com.petproject.spring.rest.controller;
 
 import com.petproject.spring.rest.entity.Employee;
-import com.petproject.spring.rest.exception_handling.EmployeeIncorrectData;
 import com.petproject.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.petproject.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,23 +28,5 @@ public class MyRESTController {
             throw new NoSuchEmployeeException("There is no employee with ID = " + id + " in Database!");
         }
         return employee;
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handleException(
-            NoSuchEmployeeException exception
-    ) {
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
-        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handleException(
-            Exception exception
-    ) {
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
-        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
     }
 }
